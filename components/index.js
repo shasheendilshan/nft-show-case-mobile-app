f0a82ca8c072856a27e6c3a500c2e7c32ba6eea1
unknown <shasheen@i-threesixty.com>
2022-04-04 19:24:40 +0530

    initial  commit

---
+++ components/index.js
@@ -0,0 +1,22 @@
+import { CircleButton, RectButton } from "./Button.js";
+import HomeHeader from "./HomeHeader.js";
+import NFTCard from "./NFTCard.js";
+import FocusedStatusBar from "./FocusedStatusBar.js";
+import DetailsBid from "./DetailsBid.js";
+import DetailsDesc from "./DetailsDesc.js";
+import { SubInfo, People, EndDate, EthPrice, NFTTitle } from "./SubInfo";
+
+export {
+  CircleButton,
+  HomeHeader,
+  NFTCard,
+  FocusedStatusBar,
+  RectButton,
+  DetailsBid,
+  DetailsDesc,
+  SubInfo,
+  EthPrice,
+  NFTTitle,
+  People,
+  EndDate,
+};
